470c2fe7fd98875543b1df86cd880059a108d67b
Andrei Istrate <64600491+istrateandrei26@users.noreply.github.com>
2021-07-05 13:00:40 +0300

    Add files via upload

---
+++ Server/Factory_FilesManager.cpp
@@ -0,0 +1,7 @@
+#include "IFilesManager.h"
+#include  "CFilesManager.h"
+
+std::unique_ptr<IFilesManager> Factory_FilesManager::Create_FilesManager() {
+
+	return std::make_unique<CFilesManager>();
+}
