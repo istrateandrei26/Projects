4bad34588e81a694c9e4698da460f6301046c28d
Andrei Istrate <64600491+istrateandrei26@users.noreply.github.com>
2021-07-05 13:05:18 +0300

    Delete Client_Library directory

--- Client_Library/Client_Library/Factory_FilesManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "IFilesManager.h"
-#include  "CFilesManager.h"
-
-std::unique_ptr<IFilesManager> Factory_FilesManager::Create_FilesManager() {
-
-	return std::make_unique<CFilesManager>();
-}
