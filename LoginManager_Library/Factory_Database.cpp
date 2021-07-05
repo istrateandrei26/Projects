d73382f4bcc467466c2e6b4e67aac614a96c185d
Andrei Istrate <64600491+istrateandrei26@users.noreply.github.com>
2021-07-05 12:49:29 +0300

    Delete LoginManager_Library directory

--- LoginManager_Library/Factory_Database.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "iDatabase.h"
-#include "CDatabase.h"
-
-iDatabase* Factory_Database::Create_DatabaseInstance() {
-
-	return new CDatabase();
-}
--- LoginManager_Library/MACRO_Database.h
+++
@@ -1,6 +0,0 @@
-#define DB_SUCCES 0
-#define DB_ERROR 1
-#define DB_ERROR_REGISTER 2
-#define DB_ERROR_QUERY 3
-#define DB_ERROR_DELETE 4
-#define DB_ERROR_UPDATE 5
